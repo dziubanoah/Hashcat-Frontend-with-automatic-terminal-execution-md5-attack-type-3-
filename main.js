d48fd6213633bc29b481d33efc4b7f6845686129
dziubanoah <dziubanoah@gmail.com>
2024-11-12 09:28:52 +0100

    v1.3

--- main.js
+++ main.js
@@ -3,10 +3,20 @@ let Passwort = document.getElementById("Input_PW");
 
 submit.addEventListener("click", function(){
     let Value = Passwort.value;
-    window.alert(Value)
-    openCMD;
+    window.alert(Value);
+    one();
 });
-function openCMD() {
-    var oShell = new ActiveXObject("WScript.Shell");
-    oShell.Run("cmd.exe /k dir");
-  }
+
+function one () {
+const fs = require('fs')
+
+// Data which will write in a file.
+let data = "Learning how to write in a file."
+
+// Write data in 'Output.txt' .
+fs.writeFile('output.txt', data, (err) => {
+
+    // In case of a error throw err.
+    if (err) throw err;
+})
+    };
