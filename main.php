dc9da055514e48f4fdae3599977bab5aa0f13328
dziubanoah <dziubanoah@gmail.com>
2024-11-18 11:14:05 +0100

    v1.6

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 
 <?php
 // Beispiel: 'dir' in CMD ausführen
-exec('cmd.exe /c ipconfig', $output);
+exec('cmd.exe /c dir', $output);
 exec('cmd.exe /c cd C:\Users\noah\Desktop\OPEN-TERMINAL-WITH-PHP-JS-HTML & node testnode.js', $output);
 
     echo implode("\n", $output); // Ausgabe des Befehls
