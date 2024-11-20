1534c6f85b92b945ef6255df8e90e0321efa2242
dziubanoah <dziubanoah@gmail.com>
2024-11-20 09:16:39 +0100

    v2.1

--- main.php
+++ main.php
@@ -6,4 +6,7 @@ exec('cmd.exe /c ipconfig', $output);
 //exec('cmd.exe /c cd C:\Users\noah\Desktop\OPEN-TERMINAL-WITH-PHP-JS-HTML & node testnode.js', $output);
 
 echo '<pre>' . htmlspecialchars(implode("\n", $output)) . '</pre>';
+
+//Momentane ausführung besser über node js, so wird auch
+//ein CMD window geöffnet
 ?>
