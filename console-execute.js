f32e21e18b89fd8e1068332b10655fa31b93dc2e
dziubanoah <dziubanoah@gmail.com>
2024-11-25 15:53:37 +0100

    v2.9

--- console-execute.js
+++ console-execute.js
@@ -5,9 +5,9 @@ fs.readFile('basiccode.txt', 'utf8', (err, data1) => {
   const Basiccode = data1.trim();
   fs.readFile('Hashing_Output.txt', 'utf8', (err, data2) => {
     const Basiccode2 = data2.trim();
-    fs.readFile('readvalue3.txt', 'utf8', (err, data3) => {
-      const Basiccode3 = data3.trim();
-      exec(`start cmd.exe /K ${Basiccode1} && ${Basiccode2} && ${Basiccode3}`);
+    fs.readFile('PWLength.txt', 'utf8', (err, data3) => {
+      const Basiccode3 = data3.trim()
+      exec(`start cmd.exe /K ${Basiccode} && ${Basiccode2} && ${Basiccode3}`);
     });
   });
 });
