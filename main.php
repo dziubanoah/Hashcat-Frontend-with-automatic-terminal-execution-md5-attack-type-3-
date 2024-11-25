0fbec7feb2fb3b29b1e7bf1ee45fa9401365de55
dziubanoah <dziubanoah@gmail.com>
2024-11-25 14:33:45 +0100

    v2.8

--- main.php
+++ main.php
@@ -1,4 +1,12 @@
+<P>Output von IP Config unter Windows:    </P>
+
 <?php
-exec('cmd.exe /c dir', $output);
+// Beispiel: 'dir' in CMD ausführen
+exec('cmd.exe /c ipconfig', $output);
+//exec('cmd.exe /c cd C:\Users\noah\Desktop\OPEN-TERMINAL-WITH-PHP-JS-HTML & node testnode.js', $output);
+
 echo '<pre>' . htmlspecialchars(implode("\n", $output)) . '</pre>';
+
+//Momentane ausführung besser über node js, so wird auch
+//ein CMD window geöffnet
 ?>
