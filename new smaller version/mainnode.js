ef7d1ec127b72a5932ed1319ea4e215b28c86818
dziubanoah <145217862+dziubanoah@users.noreply.github.com>
2025-02-10 16:39:18 +0100

    v1.4

--- new smaller version/mainnode.js
+++ new smaller version/mainnode.js
@@ -4,13 +4,25 @@ import cors from "cors"
 function requestlistener(request, response){
     response.setHeader('Access-Control-Allow-Origin', '*');
     response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
     if (request.method == "OPTIONS") {
         console.log("OPTIONS bekommen");
         console.log(request.method);
+        response.writeHead(200);
+        response.end();
     }
     else if (request.method == "POST") {
         console.log("POST Bekommen");
         console.log(request.method);
+
+        let body = ''; // Datenverarbeitung
+        request.on("data", chunk => {
+            body += chunk // empfangene Daten werden zu Body hinzugefügt
+        });
+        request.on("end", () => { // wenn der req vollständig war, wird der wert ausgegeben
+            console.log("empfangene Datei: " + body)
+        });
     }
     else {
         console.log("hab was anderes gefunden.");
