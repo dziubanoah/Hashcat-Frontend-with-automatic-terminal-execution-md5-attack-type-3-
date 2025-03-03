7addc367969d6aedceba6651ddfd52d56fcdfa30
dziubanoah <145217862+dziubanoah@users.noreply.github.com>
2025-03-03 11:32:11 +0100

    Update mainnode.js

--- new smaller version/main.js
+++ new smaller version/main.js
@@ -6,16 +6,28 @@ submit.addEventListener("click", function() {
 });
 
 function giveValue() {
+    // hier wird die Passwortlänge festgelegt, damit die HashCat Befehlssyntax korrekt gebildet werden kann.
+    let pwData_Laenge = pw.value.length
+    console.log(pwData_Laenge);
+    let pwData_laenge_for_hashcat = "?a".repeat(pwData_Laenge);
+    console.log(pwData_laenge_for_hashcat);
 
-    let pwData = pw.value
+    let pwData = pw.value;
+    let pwData_md5 = CryptoJS.MD5(pwData).toString();
+    console.log(pwData_md5);
+
     fetch("http://localhost:8080", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",  // Header für JSON-Daten
         },
-        body: JSON.stringify({Passwort: pwData})
+        body: JSON.stringify({
+            Passwort_verschlüsselt: pwData_md5,
+            Passwort_Laenge_for_hashcat: pwData_laenge_for_hashcat
+        })
     })
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error("Error: ", error));
+    console.log("DIe Payload ist: " + pwData)
 }
