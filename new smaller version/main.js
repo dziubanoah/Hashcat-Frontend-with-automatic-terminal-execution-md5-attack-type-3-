e7aea00b80896d38713570afd8050c38e99bea02
dziubanoah <145217862+dziubanoah@users.noreply.github.com>
2025-05-12 14:52:26 +0200

    new comments / no individual guess mask

--- new smaller version/main.js
+++ new smaller version/main.js
@@ -7,21 +7,19 @@ submit.addEventListener("click", function() {
 
 function giveValue() {
     // hier wird die Passwortlänge festgelegt, damit die HashCat Befehlssyntax korrekt gebildet werden kann.
-    let pwData_Laenge = pw.value.length
-    console.log(pwData_Laenge);
-    let pwData_laenge_for_hashcat = "?a".repeat(pwData_Laenge);
+    let pwData_laenge_for_hashcat = "?u?a?a?a?a?a?a?s"
     console.log(pwData_laenge_for_hashcat);
-
+    //Hier wird das eingegebene Passwort verschlüsselt.
     let pwData = pw.value;
     let pwData_md5 = CryptoJS.MD5(pwData).toString();
     console.log(pwData_md5);
 
-    fetch("http://localhost:8080", {
+    fetch("http://localhost:8080", { //verschickt die daten per fetch an den Node Server
         method: "POST",
         headers: {
             "Content-Type": "application/json",  // Header für JSON-Daten
         },
-        body: JSON.stringify({
+        body: JSON.stringify({ //Daten, die als JSON verschickt werden.
             Passwort_verschlüsselt: pwData_md5,
             Passwort_Laenge_for_hashcat: pwData_laenge_for_hashcat
         })
